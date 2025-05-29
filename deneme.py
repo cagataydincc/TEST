37424a94b9a370e40d6a48b55a802e88d840af80
CAGATAY DINC <cagatay210@hotmail.com>
2025-05-29 14:08:51 +0300

    lk commit: proje dosyaları eklendi

---
+++ deneme.py
@@ -0,0 +1,13 @@
+import pytest
+from playwright.sync_api import sync_playwright
+
+def testlog():
+  with sync_playwright() as p:
+      browser = p.chromium.launch()
+      page = browser.new_page()
+      page.goto("https://demo.automationtesting.in/Index.html")
+
+
+
+
+
---
+++ test_trycatch.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright, expect
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page(record_video_dir='./video')
+    page.goto('https://demo.automationtesting.in/Selectable.html')
+    page.wait_for_timeout(1000)
+    try:
+        elements=page.query_selector_all('a')
+        print(len(elements))
+        for i in elements:
+            print(i.text_content())
+        page.wait_for_timeout(1000)
+
+    except Exception as e:
+        print("⛔ Sayfa yüklenirken beklenmeyen bir hata oluştu:",str(e))
+   
